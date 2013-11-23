1d161a7e1d44202621e060ccadef57a5ac1fa7cf
Constantin Șerban-Rădoi <costashsrc@gmail.com>
2013-11-23 17:41:08 +0200

    Some test for 20+ cards.

--- public/js/cards.js
+++ public/js/cards.js
@@ -8,6 +8,7 @@ function loadImage(URL, imageObj) {
 
 var SUITS = ['C', 'S', 'H', 'D'];
 var RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'];
+var cards = [];
 
 var Card = function(rank, suit) {
 	var card = document.createElement('div');
@@ -37,6 +38,18 @@ Card.prototype.setImagePos = function setImagePos(x, y) {
 	this.card.style.backgroundPositionY = -y * 98 + 'px';
 }
 
-var c = new Card('J', 'D');
+cards.push(new Card('J', 'D'));
+cards.push(new Card('A', 'H'));
+cards.push(new Card('T', 'S'));
+cards.push(new Card('4', 'C'));
+for (var i = 0; i < 20; ++i) {
+	cards.push(new Card(RANKS[i % 13], SUITS[i % 4]));
+}
 
-$('#container').append(c.card);
+function attachToDom(cards) {
+	for(var i = 0; i < cards.length; ++i) {
+		$('#container').append(cards[i].card);
+	}
+}
+
+attachToDom(cards);
