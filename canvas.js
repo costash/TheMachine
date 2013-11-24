e4c5612327ade1e47a7fd9c07cec0eb0771b5a95
tudalex <tudalex@gmail.com>
2013-11-24 07:36:14 +0000

    New canvas implementation.

--- canvas.js
+++ canvas.js
@@ -4,17 +4,15 @@ var fs = require('fs'),
     canvas= new Canvas(1920, 1080),
     ctx = canvas.getContext('2d'),
     ue = require('../node-openvg-canvas/examples/util'),
-    cards = fs.readFileSync('public/res/cards.png'),
-    arrow = fs.readFileSync('public/res/arrow.png'),
+    cards = fs.readFileSync('public/img/cards.png'),
+    arrow = fs.readFileSync('public/img/arrow.png'),
     ip = require('os').networkInterfaces().eth0[0].address;
 
 function initCanvas() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.fillStyle='#355e3b';
 	ctx.save();
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-		
+	ctx.fillRect(0, 0, canvas.width, canvas.height);		
 	canvas.vgSwapBuffers();
 	
 }
@@ -34,23 +32,45 @@ function drawInstance(players, card, umflaturi, current_player) {
 	
 	//Users
 	ctx.font = '48pt Arial';
-	ctx.fillStyle='blue';
+	ctx.fillStyle='#002800';
 	ctx.textBaseline = 'center';
 	ctx.textAlign = 'left';
 	ctx.strokeStyle='blue';	
 	for (var i = 0; i < players.length; ++i)
 		ctx.fillText(players[i],400, 200+i*100);
-	img.src = arrow;
-	ctx.drawImage(img, 200,100+ current_player*100);
+	if (current_player > -1) {
+		current_player = current_player%players.length;
+		img.src = arrow;
+		ctx.drawImage(img, 200,100+ current_player*100);
+	}
+
+	ctx.strokeStyle='red';
+	ctx.fillStyle='red';
+	ctx.fillText('Umflaturi: '+umflaturi, 1200, 200);
+	
 
 	//Swap them buffers
 	canvas.vgSwapBuffers();
-
-
-	
 }
 
 initCanvas();
 
-drawInstance(['Nicu', 'Alex', 'Tataie'], { type:0, nr: 0}, 0, 0);
+//drawInstance(['Nicu', 'Alex', 'Tataie'], { type:0, nr: 0}, 20, 10);
+
+function drawWinScreen(players) {
+	initCanvas();
+	ctx.font = '50pt Arial';
+	ctx.fillStyle = '#E82020';
+	ctx.textBaseline = 'center';
+	ctx.textAlign = 'center';
+	ctx.fillText('Clasament', canvas.width/2, 200);
+	ctx.font = '38pt Arial';
+	ctx.fillStyle = '#0D0D90';
+	ctx.strokeStyle = '#0D0D90';
+	for (var i = 0 ; i < players.length; ++ i) 
+		ctx.fillText((i+1).toString()+'. '+players[i], canvas.width/2, 400+i*100);
+	canvas.vgSwapBuffers();	
+}
+
+drawWinScreen(['Tataie', 'Nicu', 'Alex']);
 ue.waitForInput();
